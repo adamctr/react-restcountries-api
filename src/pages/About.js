cdfb0e732fa903c790bfad00556d75f6399a9aef
Adam <adam.courtaro@gmail.com>
2022-04-25 02:10:05 +0200

    first draft

---
+++ src/pages/About.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae
+      tempora beatae eius quod officia magnam provident? Veniam blanditiis
+      laborum illum mollitia aspernatur, amet porro, accusamus assumenda numquam
+      aliquam repellat laboriosam alias sit eligendi, asperiores earum dolorum
+      magnam! Ex, soluta perferendis! Inventore doloribus sed tempora nesciunt
+      neque distinctio, commodi repellat numquam libero aperiam error dolore
+      corrupti quis asperiores sunt amet. Dolor eos deserunt animi vitae veniam
+      laudantium expedita itaque culpa, tenetur qui quidem laboriosam ab nobis
+      voluptatibus dolore ducimus, nam necessitatibus saepe quae? Sequi deserunt
+      ipsa nihil ut maiores! Voluptates ea consequuntur neque ducimus at
+      aspernatur commodi beatae consequatur rerum deleniti!
+    </div>
+  );
+};
+
+export default About;
